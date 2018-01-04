fbd2b0ad759800394c258aebeaaf2a3ae5d3101d
mkg20001 <mkg20001@gmail.com>
2018-01-04 18:31:30 +0100

    misc: Updates & codestyle

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,85 @@
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-const del = require('del');
-const runSequence = require('run-sequence');
+const gulp = require('gulp')
+const $ = require('gulp-load-plugins')()
+const del = require('del')
+const runSequence = require('run-sequence')
 
-const dist = "dist"
+const dist = 'dist'
 
-gulp.task("clean", function () {
+gulp.task('clean', function () {
   return del(dist)
 })
 
-gulp.task("devJS", function () {
-  gulp.src("src/*.coffee")
+gulp.task('devJS', function () {
+  gulp.src('src/*.coffee')
     .pipe($.sourcemaps.init())
     .pipe($.coffee())
-    .pipe($.concat("zeronet-notifications.js"))
+    .pipe($.concat('zeronet-notifications.js'))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest(dist))
 })
 
-gulp.task("js", ["jsmin"], function () {
-  gulp.src("src/*.coffee")
-    //Build *.coffee files & sourcemaps => js
+gulp.task('js', ['jsmin'], function () {
+  gulp.src('src/*.coffee')
+    // Build *.coffee files & sourcemaps => js
     .pipe($.sourcemaps.init())
     .pipe($.coffee())
-    .pipe($.concat("zeronet-notifications.js"))
-    .pipe($.sourcemaps.write("."))
+    .pipe($.concat('zeronet-notifications.js'))
+    .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(dist))
 })
 
-gulp.task("jsmin", function () {
-  gulp.src("src/*.coffee")
-    //Build *.coffee files & sourcemaps => js
+gulp.task('jsmin', function () {
+  gulp.src('src/*.coffee')
+    // Build *.coffee files & sourcemaps => js
     .pipe($.sourcemaps.init())
     .pipe($.coffee())
-    .pipe($.concat("zeronet-notifications.js"))
-    //Uglifiy => min.js
-    .pipe($.concat("zeronet-notifications.min.js"))
+    .pipe($.concat('zeronet-notifications.js'))
+    // Uglifiy => min.js
+    .pipe($.concat('zeronet-notifications.min.js'))
     .pipe($.uglify())
-    .pipe($.sourcemaps.write("."))
+    .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(dist))
 })
 
-gulp.task("allJS", function () {
-  gulp.src(["bower_components/jquery/dist/jquery.min.js", "bower_components/jquery.easing/js/jquery.easing.min.js", "src/*.coffee"])
-    //Build *.coffee files, add libs & uglify => all.min.js
+gulp.task('allJS', function () {
+  gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/jquery.easing/js/jquery.easing.min.js', 'src/*.coffee'])
+    // Build *.coffee files, add libs & uglify => all.min.js
     .pipe($.sourcemaps.init())
-    .pipe($.if("*.coffee", $.coffee()))
+    .pipe($.if('*.coffee', $.coffee()))
     .pipe($.uglify())
-    .pipe($.concat("zeronet-notifications.all.min.js"))
-    .pipe($.sourcemaps.write("."))
+    .pipe($.concat('zeronet-notifications.all.min.js'))
+    .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(dist))
 })
 
-gulp.task("devCSS", function () {
-  gulp.src("src/*.css")
-    .pipe($.concat("zeronet-notifications.css"))
+gulp.task('devCSS', function () {
+  gulp.src('src/*.css')
+    .pipe($.concat('zeronet-notifications.css'))
     .pipe(gulp.dest(dist))
 })
 
-gulp.task("css", ["cssmin"], function () {
-  gulp.src("src/*.css")
+gulp.task('css', ['cssmin'], function () {
+  gulp.src('src/*.css')
     .pipe($.sourcemaps.init())
-    .pipe($.concat("zeronet-notifications.css"))
-    .pipe($.sourcemaps.write("."))
+    .pipe($.concat('zeronet-notifications.css'))
+    .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(dist))
 })
 
-gulp.task("cssmin", function () {
-  gulp.src("src/*.css")
+gulp.task('cssmin', function () {
+  gulp.src('src/*.css')
     .pipe($.sourcemaps.init())
-    .pipe($.concat("zeronet-notifications.css"))
+    .pipe($.concat('zeronet-notifications.css'))
     .pipe($.cleanCss())
-    .pipe($.concat("zeronet-notifications.min.css"))
-    .pipe($.sourcemaps.write("."))
+    .pipe($.concat('zeronet-notifications.min.css'))
+    .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest(dist))
 })
 
-gulp.task("dev", ["clean"], function () {
-  return runSequence(["devJS", "devCSS"])
+gulp.task('dev', ['clean'], function () {
+  return runSequence(['devJS', 'devCSS'])
 })
 
-gulp.task("default", ["clean"], function () {
-  return runSequence(["js", "allJS", "css"])
+gulp.task('default', ['clean'], function () {
+  return runSequence(['js', 'allJS', 'css'])
 })
